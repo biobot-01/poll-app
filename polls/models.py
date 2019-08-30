1eb5350ec1401927022c9288e8f0baa521ca0dae
Kevin Michael <biobot.mike@gmail.com>
2019-08-30 20:34:48 +0200

    Add recently published method

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -10,6 +12,10 @@ class Question(models.Model):
     def __str__(self):
         """Return question text."""
         return self.question_text
+
+    def was_published_recently(self):
+        """Return recently published object."""
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
