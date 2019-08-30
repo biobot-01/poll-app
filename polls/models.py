568713ba02fe5d72abe46bba7d66f798d5228b05
Kevin Michael <biobot.mike@gmail.com>
2019-08-30 20:27:57 +0200

    Add string method

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,10 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        """Return question text."""
+        return self.question_text
+
 
 class Choice(models.Model):
     """Choice model."""
@@ -14,3 +18,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """Return choice text."""
+        return self.choice_text
