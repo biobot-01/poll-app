e036c04deba7d22431d46b8176422a34a12b2431
Kevin Michael <biobot.mike@gmail.com>
2019-08-31 07:33:53 +0200

    Add views & register them

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,18 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('This is the polls app index function for views.')
+
+
+def detail(request, question_id):
+    response = f"You're looking at question {question_id}"
+    return HttpResponse(response)
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    response = f"You're voting on question {question_id}"
+    return HttpResponse(response)
