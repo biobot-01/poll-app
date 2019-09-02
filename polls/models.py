92587037f05a5b12f34ecbbc2a8efd5f0005892c
Kevin Michael <biobot.mike@gmail.com>
2019-09-02 20:25:27 +0200

    Fix bug in was_published_recently

--- polls/models.py
+++ polls/models.py
@@ -15,7 +15,8 @@ class Question(models.Model):
 
     def was_published_recently(self):
         """Return recently published object."""
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
