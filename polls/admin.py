614bc460ce1dd2aae0c7fc2512caf00d46e54519
Kevin Michael <biobot.mike@gmail.com>
2019-09-03 20:46:58 +0200

    Change order of admin question fields

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    """Customize Question Model Admin."""
+
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
